9ec87144ccfe7532ad975308416eb98026f5c5d3
josubernal <josubernaldeuribarri@gmail.com>
2026-04-26 17:18:29 +0200

    to

--- benchmarks/layer_order.py
+++ benchmarks/layer_order.py
@@ -5,8 +5,7 @@ import configparser
 import torch.nn.functional as F
 from admm import (
     ADMM_SpikingLinear, ADMM_Flatten, ADMM_SpikingConv2d, 
-    ADMM_Conv2d, ADMM_Linear, ADMM, ADMM_Heaviside, ADMM_ReLU, ADMM_Metrics, ADMM_Hinge
-)
+    ADMM_Conv2d, ADMM_Linear, ADMM, ADMM_Heaviside, ADMM_ReLU, ADMM_Metrics, ADMM_CrossEntropy_Taylor,ADMM_SSE)
 
 import json
 import os
@@ -82,10 +81,10 @@ for model_name in model_types:
         # DATA
         if model_name in ["linear", "conv"]:
             batch_size= batch_size_static
-            images, labels= get_data(batch_size, spiking=False, device=device)
+            images, labels= get_data(batch_size, spiking=False, device=device, seed=seed)
         else:
             batch_size= batch_size_spiking
-            images, labels = get_data(batch_size, spiking=True, device=device, n_timesteps=n_timesteps)
+            images, labels = get_data(batch_size, spiking=True, device=device, n_timesteps=n_timesteps, seed=seed)
             
         labels_one_hot = F.one_hot(labels.long(), num_classes=10).float()
 
@@ -97,7 +96,7 @@ for model_name in model_types:
                     ADMM_Linear(in_f=input_size, out_f=hidden_size_static, h=ADMM_ReLU(), init="pytorch", bias=True),
                     ADMM_Linear(in_f=hidden_size_static, out_f=10, h=ADMM_ReLU(), init="pytorch", bias=True)
                 ])
-                admm_model = ADMM(linear_layers, rho=linear_rho, beta=linear_beta, loss_f=ADMM_Hinge(),init="pytorch", bias=True,layer_order=layer_order, train_method='vectorized').to(device)
+                admm_model = ADMM(linear_layers, rho=linear_rho, beta=linear_beta, loss_f=ADMM_SSE(),init="pytorch", bias=True,layer_order=layer_order, train_method='vectorized').to(device)
                 images = images.view(images.size(0), -1)
 
             case "conv":
@@ -107,13 +106,13 @@ for model_name in model_types:
                     ADMM_Conv2d(in_c=1, out_c=hidden_channels_static, k=k, p=p, s=s, h=ADMM_ReLU(), init="pytorch", bias=True, use_fft=False, padding_mode='zeros'),
                     ADMM_Linear(in_f=lin_in_dim, out_f=10, h=ADMM_ReLU(),pool_op=ADMM_Flatten(), init="pytorch", bias=True)
                 ])
-                admm_model = ADMM(conv_layers, rho=conv_rho, beta=conv_beta,loss_f=ADMM_Hinge(), init="pytorch", bias=True, layer_order=layer_order, train_method='vectorized').to(device) 
+                admm_model = ADMM(conv_layers, rho=conv_rho, beta=conv_beta,loss_f=ADMM_SSE(), init="pytorch", bias=True, layer_order=layer_order, train_method='vectorized').to(device) 
             case "spiking-linear":
                 splinear_layers = nn.ModuleList([ 
                     ADMM_SpikingLinear(in_f=34*34*2, out_f=hidden_size_spiking, h=ADMM_Heaviside(thetas=thetas), init="s-uniform", bias=False),
                     ADMM_SpikingLinear(in_f=hidden_size_spiking, out_f=10, h=None, init="s-uniform", bias=False)
                 ])
-                admm_model = ADMM(splinear_layers, T=n_timesteps, rho=splinear_rho, thetas=thetas, deltas=deltas, beta=splinear_beta, loss_f=ADMM_Hinge(),init="s-uniform",layer_order=layer_order, bias=False, train_method='decoupled-backwards').to(device)
+                admm_model = ADMM(splinear_layers, T=n_timesteps, rho=splinear_rho, thetas=thetas, deltas=deltas, beta=splinear_beta, loss_f=ADMM_CrossEntropy_Taylor(),init="s-uniform",layer_order=layer_order, bias=False, train_method='decoupled-backwards').to(device)
                 images = images.view(images.size(0), images.size(1), -1).permute(1, 0, 2)
             case "spiking-conv":
                 spatial_dim = int(calc_spatial_out(34, k, p, s))
@@ -122,7 +121,7 @@ for model_name in model_types:
                     ADMM_SpikingConv2d(in_c=2, out_c=hidden_channels_spiking, k=k, p=p, s=s, h=ADMM_Heaviside(thetas=thetas), init="s-uniform", bias=False, use_fft=False,  padding_mode="zeros"),
                     ADMM_SpikingLinear(in_f=lin_in_dim, pool_op=ADMM_Flatten(), out_f=10, h=None, init="s-uniform", bias=False)
                 ])
-                admm_model = ADMM(spconv_layers, T=n_timesteps, rho=spconv_rho, thetas=thetas, deltas=deltas, beta=spconv_beta, loss_f=ADMM_Hinge(), init="s-uniform", layer_order=layer_order, bias=False, train_method='decoupled-backwards').to(device)
+                admm_model = ADMM(spconv_layers, T=n_timesteps, rho=spconv_rho, thetas=thetas, deltas=deltas, beta=spconv_beta, loss_f=ADMM_CrossEntropy_Taylor(), init="s-uniform", layer_order=layer_order, bias=False, train_method='decoupled-backwards').to(device)
                 images = images.permute(1, 0, 2, 3, 4)
         #########################################
         # ADMM TRAINING LOOP
