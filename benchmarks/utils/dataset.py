1e3210f6621de77a02286e6b953c90a98d876325
josubernal <josubernaldeuribarri@gmail.com>
2026-05-28 12:58:19 +0200

    dataset bug solved

--- benchmarks/utils/dataset.py
+++ benchmarks/utils/dataset.py
@@ -2,15 +2,8 @@ import tonic
 import tonic.transforms as tr
 import torch
 from tonic import DiskCachedDataset
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-
-
-def one_hot_target(y, num_classes=10):
-    """Converts a standard integer label into a one-hot float tensor."""
-    return torch.nn.functional.one_hot(
-        torch.tensor(y, dtype=torch.long), num_classes=num_classes
-    ).float()
 
 
 def format_images(images, model_name):
@@ -23,37 +16,6 @@ def format_images(images, model_name):
         images = images.view(images.size(0), -1)
 
     return images.contiguous()
-
-
-def spiking_collate_fn(batch, n_timesteps, model_name):
-    """Custom collate function to handle ADMM's [Time, Batch, ...] requirements."""
-    dummy_batch = [(data, 0) for data, label in batch]
-    # Explicitly enforce batch_first=False to guarantee [Time, Batch, C, H, W]
-    images, _ = tonic.collation.PadTensors(batch_first=False)(dummy_batch)
-
-    images += 0.01 * torch.randn_like(images)
-
-    clean_labels = []
-    for _, label in batch:
-        t = label if isinstance(label, torch.Tensor) else torch.tensor(label)
-        if t.dim() > 0 and t.size(-1) > 1:
-            t = t.argmax()
-        clean_labels.append(t.to(torch.long))
-
-    labels = torch.stack(clean_labels)
-
-    # 3. Handle timestep clipping/padding as usual
-    if images.size(0) > n_timesteps:
-        images = images[:n_timesteps]
-    elif images.size(0) < n_timesteps:
-        pad_size = n_timesteps - images.size(0)
-        padding = torch.zeros(pad_size, *images.shape[1:], dtype=images.dtype)
-        images = torch.cat([images, padding], dim=0)
-
-    # Apply architecture-specific formatting
-    images = format_images(images, model_name)
-
-    return images, labels
 
 
 def get_dataset(model_name, batch_size, n_batches, seed=64, n_timesteps=150):
@@ -72,20 +34,25 @@ def get_dataset(model_name, batch_size, n_batches, seed=64, n_timesteps=150):
             transform=frame_transform,
             train=True,
         )
-
-        subset_indices = list(range(batch_size * n_batches))
-        trainset = Subset(trainset, subset_indices)
-
         cached_trainset = DiskCachedDataset(trainset, cache_path="./cache/nmnist/train")
         train_loader = DataLoader(
             cached_trainset,
             batch_size=batch_size,
-            collate_fn=lambda b: spiking_collate_fn(b, n_timesteps, model_name),
+            collate_fn=tonic.collation.PadTensors(),
             shuffle=True,
             drop_last=True,
             generator=torch.Generator().manual_seed(seed),
         )
-        return train_loader
+
+        data, targets = next(iter(train_loader))
+        data = format_images(data, model_name)
+
+        if data.size(1) > n_timesteps:
+            data = data[:, :n_timesteps, :]
+
+        data = data.permute(1, 0, 2)
+        data += 0.01 * torch.randn_like(data)
+        return [(data, targets)]
 
     else:
         transform = transforms.Compose(
@@ -96,63 +63,25 @@ def get_dataset(model_name, batch_size, n_batches, seed=64, n_timesteps=150):
             root="./data",
             train=True,
             download=True,
-            target_transform=one_hot_target,
             transform=transform,
         )
-
-        subset_indices = list(range(batch_size * n_batches))
-        trainset = Subset(trainset, subset_indices)
-
-        # For static models, we format within a custom collate function
-        def static_collate_fn(batch):
-            images = torch.stack([item[0] for item in batch])
-            labels = torch.stack([item[1] for item in batch])
-            images = format_images(images, model_name)
-            return images, labels
 
         train_loader = DataLoader(
             trainset,
             batch_size=batch_size,
             shuffle=True,
             drop_last=True,
-            collate_fn=static_collate_fn,
         )
-        return train_loader
+        data, targets = next(iter(train_loader))
+        data = format_images(data, model_name)
+        return [(data, targets)]
 
 
 #############################################
 # FOR GD
-
-
-def gd_collate_fn(batch, n_timesteps, model_name):
-    dummy_batch = [(data, 0) for data, label in batch]
-    # Guarantee [T, B, C, H, W] for consistency
-    images, _ = tonic.collation.PadTensors(batch_first=False)(dummy_batch)
-
-    raw_labels = [label for data, label in batch]
-    clean_labels = []
-    for label in raw_labels:
-        t = label if isinstance(label, torch.Tensor) else torch.tensor(label)
-        if t.dim() == 0:
-            t = torch.nn.functional.one_hot(t.to(torch.long), num_classes=10).float()
-        clean_labels.append(t)
-
-    labels = torch.stack(clean_labels)
-
-    # Truncate or Pad time dimension (dim 0)
-    if images.size(0) > n_timesteps:
-        images = images[:n_timesteps]
-    elif images.size(0) < n_timesteps:
-        pad_size = n_timesteps - images.size(0)
-        padding = torch.zeros(pad_size, *images.shape[1:], dtype=images.dtype)
-        images = torch.cat([images, padding], dim=0)
-
-    images = format_images(images, model_name)
-    return images, labels
-
-
 def get_data(model_name, batch_size, device=None, n_timesteps=150, seed=64):
     spiking = model_name.startswith("spiking")
+
     if spiking:
         sensor_size = tonic.datasets.NMNIST.sensor_size
         frame_transform = tr.Compose(
@@ -162,20 +91,29 @@ def get_data(model_name, batch_size, device=None, n_timesteps=150, seed=64):
             ]
         )
         trainset = tonic.datasets.NMNIST(
-            save_to="./data", transform=frame_transform, train=True
+            save_to="./data",
+            transform=frame_transform,
+            train=True,
         )
         cached_trainset = DiskCachedDataset(trainset, cache_path="./cache/nmnist/train")
         train_loader = DataLoader(
             cached_trainset,
             batch_size=batch_size,
-            collate_fn=lambda b: gd_collate_fn(b, n_timesteps, model_name),
+            collate_fn=tonic.collation.PadTensors(),
             shuffle=True,
             drop_last=True,
             generator=torch.Generator().manual_seed(seed),
         )
 
-        images, labels = next(iter(train_loader))
+        data, targets = next(iter(train_loader))
+        data = format_images(data, model_name)
 
+        if data.size(1) > n_timesteps:
+            data = data[:, :n_timesteps, :]
+
+        data = data.permute(1, 0, 2)
+        data += 0.01 * torch.randn_like(data)
+        return data.to(device), targets.to(device)
     else:
         transform = transforms.Compose(
             [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
@@ -190,10 +128,8 @@ def get_data(model_name, batch_size, device=None, n_timesteps=150, seed=64):
         images, labels = next(iter(dataloader))
         images = format_images(images, model_name)
 
-    images = images.to(device).float()
-    labels = labels.to(device)
+        images = images.to(device).float()
+        labels = labels.to(device)
+        images += 0.01 * torch.randn_like(images)
 
-    # Restored: Apply noise as done in your previous script
-    images += 0.01 * torch.randn_like(images)
-
-    return images, labels
+        return images, labels
